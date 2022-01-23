048a43e05d11dbab78a1f2ce22e19648de9713cc
Amin Zayeromali <amin.zayeromali@gmail.com>
2022-01-23 22:50:53 +0330

    New Update ...

---
+++ Short-Codes/Multiple-File-Load.py
@@ -0,0 +1,23 @@
+# Import Module
+import os
+
+# Folder Path
+path = "/home/aminzayer/Mygit/My-Python-Code/Short-Codes/"
+
+# Change the directory
+os.chdir(path)
+
+# Read text File
+def read_text_file(file_path):
+	with open(file_path, 'r') as f:
+		print(f.read())
+
+
+# iterate through all file
+for file in os.listdir():
+	# Check whether file is in text format or not
+	if file.endswith(".txt"):
+		file_path = f"{path}/{file}"
+
+		# call read text file function
+		read_text_file(file_path)
