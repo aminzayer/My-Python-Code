bd3a0af2908afcfabd10fd09ecf8f28fb55d52bd
Amin Zayeromali <amin.zayeromali@gmail.com>
2022-02-02 20:25:07 +0330

    New Update & add ...

---
+++ Scrape-Google-Result.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+import urllib
+from fake_useragent import UserAgent
+import requests
+import re
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup
+
+keyword = "Amin Zayeromali"
+html_keyword = urllib.parse.quote_plus(keyword)
+print(html_keyword)
+
+number_of_result = 20
+google_url = "https://www.google.com/search?q=" + \
+    html_keyword + "&num=" + str(number_of_result)
+print(google_url)
+
+ua = UserAgent()
+response = requests.get(google_url, {"User-Agent": ua.random})
+soup = BeautifulSoup(response.text, "html.parser")
+
+result = soup.find_all('div', attrs={'class': 'ZINbbc'})
+results = [re.search('\/url\?q\=(.*)\&sa', str(i.find('a', href=True)['href']))
+           for i in result if "url" in str(i)]
+#this is because in rare cases we can't get the urls
+links = [i.group(1) for i in results if i != None]
+print(links)
+
