a7887c205d905eb68146fdcb19f9239beda48b9a
Amin Zayeromali <amin.zayeromali@gmail.com>
2022-02-10 00:10:08 +0330

    new update ...

--- Interview-Questions/test_unittest.py
+++ Interview-Questions/test_unittest.py
@@ -3,9 +3,9 @@ import unittest     # The test framework
 
 class Test_TestFactorial(unittest.TestCase):
     def test_factorial(self):
-        self.test_factorial(Factorial.factorial(3), 6)
-        self.test_factorial(Factorial.factorial(4), 24)
-        self.test_factorial(Factorial.factorial(4), 120)
+        self.assertEqual(Factorial.factorial(3), 6)
+        self.assertEqual(Factorial.factorial(4), 24)
+        self.assertEqual(Factorial.factorial(4), 120)
 
 if __name__ == '__main__':
     unittest.main()
