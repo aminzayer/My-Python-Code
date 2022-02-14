523f988d690a8dbfe926660c2c68af7bb82b9d95
Amin Zayeromali <amin.zayeromali@gmail.com>
2022-02-14 11:53:46 +0330

    New Update ...

---
+++ Moule-Packages/MyProgram.py
@@ -0,0 +1,10 @@
+from MyModules import *
+from submodulefolder.mysubmodule import *
+util_func()
+myinstance = UsefulClass("Hello Amin")
+myinstance.report()
+
+mybook = Person("Amin","Zayeromali")
+mybook.report()
+
+my_sub_func()
---
+++ Moule-Packages/submodulefolder/mysubmodule.py
@@ -0,0 +1,2 @@
+def my_sub_func():
+    print("Using a function from mysubmodule.py!")
