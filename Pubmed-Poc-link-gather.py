84b1b3e41f1f18f0b1fc679851f96e8c435b0747
Amin Zayeromali <amin.zayeromali@gmail.com>
2022-06-14 20:51:24 +0430

    Added New Algorithems ...

--- Pubmed-Poc-link-gather.py
+++ Pubmed-Poc-link-gather.py
@@ -1,25 +1,36 @@
 from fake_useragent import UserAgent
-import requests
+import requests,json
 from urllib.parse import quote
 from bs4 import BeautifulSoup
 
-def Gathering_Links(Count,Type,TopicSubject,Datemin,Datemax):
+
+def Gathering_Links(Count, Type, TopicSubject, Datemin, Datemax):
     Search_results = []
     UA = UserAgent(use_cache_server=False)
     term = quote(TopicSubject)
     if Type.lower() == 'pubmed':
-        response = requests.get('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term='+term+'&mindate='+Datemin+'&maxdate='+Datemax+'&retmax='+Count, headers={"User-Agent": UA.random}, timeout=(10, 30))
-        infoids=BeautifulSoup(response.content, 'xml')
-        ids = infoids.find_all('Id')
-        for id in ids:
-            Search_results.append({'Url': 'https://pubmed.ncbi.nlm.nih.gov/'+id.get_text()+'/'})
+        try:
+            response = requests.get('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term='+term +
+                                    '&mindate='+Datemin+'&maxdate='+Datemax+'&retmax='+Count, headers={"User-Agent": UA.random}, timeout=(10, 30))
+            infoids = BeautifulSoup(response.content, 'xml')
+            ids = infoids.find_all('Id')
+            for id in ids:
+                Search_results.append(
+                    {'Url': 'https://pubmed.ncbi.nlm.nih.gov/'+id.get_text()+'/'})
+        except:
+            Search_results.append({'Error': 'Error raised in Request'})
     elif Type.lower() == 'poc':
-        reponse ='do somthings'
+        try:
+            articles = requests.get(
+                'https://secureapi.atpoc.com/api-contentstream/beta/factually/', headers={"User-Agent": UA.random}).json()
+            for article in articles.values():
+                Search_results.append(
+                    {'Url': 'https://breakingmed.org/article.html?articleid=' + article['articleid']})
+        except Exception as e:
+            Search_results.append({'Error': str(e)})
     else:
-        Search_results.append(
-            {'Error': 'Type not supported'})
+        Search_results.append({'Error': 'Type not supported'})
     return Search_results
 
 
-
-print (Gathering_Links('1000','PubMed','breast cancer','2000','2020'))
+print(Gathering_Links('100', 'poc', 'breast cancer', '2000', '2020'))
