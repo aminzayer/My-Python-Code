f5e225d8a075eb66bd5b216c5c57df53f4aa818c
Amin Zayeromali <amin.zayeromali@gmail.com>
2022-12-19 13:56:52 +0330

    New Update:Semantic Model Module ...

--- SentenceTransformer-Semantic-Model.py
+++ SentenceTransformer-Semantic-Model.py
@@ -40,17 +40,17 @@ class Semantic:
     def Calculate_Semantic_Two_Sentences(self,sentence1,sentence2):
         SentEmbed1= self.SentenceEmbedding(sentence=sentence1)
         SentEmbed2= self.SentenceEmbedding(sentence=sentence2)
-        return util.cos_sim(SentEmbed1, SentEmbed2)
+        return util.cos_sim(SentEmbed1, SentEmbed2).numpy()[0][0]
 
     def Calculate_Semantic_Two_Corpus(self,corpus1,corpus2):
         CorpusEmbed1 = self.SentenceEmbedding(sentence=corpus1)
         CorpusEmbed2 = self.SentenceEmbedding(sentence=corpus2)
-        return util.cos_sim(CorpusEmbed1, CorpusEmbed2)
+        return util.cos_sim(CorpusEmbed1, CorpusEmbed2).numpy()[0][0]
     
 
 Semantic_Model = Semantic()
 emtiyaz = Semantic_Model.Calculate_Semantic_Two_Sentences(sentence1='من خوردن پیتزا را دوست دارم', sentence2='من شام خوردم')
-print(emtiyaz.numpy()[0])
+print(emtiyaz)
 
 
 
