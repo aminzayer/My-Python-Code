894b287ea036520638392bcd10351457b9168eca
Amin Zayeromali <amin.zayeromali@gmail.com>
2023-10-13 19:02:48 +0330

    Update Code ...

--- semantic-search.py
+++ semantic-search.py
@@ -1,54 +1,48 @@
-"""
-This is a simple application for sentence embeddings: semantic search
-
-We have a corpus with various sentences. Then, for a given query sentence,
-we want to find the most similar sentence in this corpus.
-
-This script outputs for various queries the top 5 most similar sentences in the corpus.
-"""
 from sentence_transformers import SentenceTransformer, util
 import torch
 
-embedder = SentenceTransformer('all-MiniLM-L6-v2')
 
-# Corpus with example sentences
-corpus = ['A man is eating food.',
-          'A man is eating a piece of bread.',
-          'The girl is carrying a baby.',
-          'A man is riding a horse.',
-          'A woman is playing violin.',
-          'Two men pushed carts through the woods.',
-          'A man is riding a white horse on an enclosed ground.',
-          'A monkey is playing drums.',
-          'A cheetah is running behind its prey.'
-          ]
-corpus_embeddings = embedder.encode(corpus, convert_to_tensor=True)
+def find_top_similar_sentences(queries, corpus, model_name='all-MiniLM-L6-v2', top_k=5):
+    # Load the sentence embedding model
+    embedder = SentenceTransformer(model_name)
 
-# Query sentences:
-queries = ['A man is eating pasta.', 'Someone in a gorilla costume is playing a set of drums.',
-           'A cheetah chases prey on across a field.']
+    # Encode the corpus sentences
+    corpus_embeddings = embedder.encode(corpus, convert_to_tensor=True)
+
+    # Find the closest k sentences in the corpus for each query sentence based on cosine similarity
+    results = []
+    for query in queries:
+        query_embedding = embedder.encode(query, convert_to_tensor=True)
+
+        # Calculate cosine similarities and retrieve top_k results
+        cos_scores = util.cos_sim(query_embedding, corpus_embeddings)[0]
+        top_results = torch.topk(cos_scores, k=min(top_k, len(corpus)))
+
+        # Store the results
+        query_results = []
+        for score, idx in zip(top_results[0], top_results[1]):
+            query_results.append((corpus[idx], score.item()))
+        results.append((query, query_results))
+
+    return results
 
 
-# Find the closest 5 sentences of the corpus for each query sentence based on cosine similarity
-top_k = min(5, len(corpus))
-for query in queries:
-    query_embedding = embedder.encode(query, convert_to_tensor=True)
+if __name__ == "__main__":
+    # Define the corpus sentences
+    corpus = [
+        'A man is eating food.', 'A man is eating a piece of bread.', 'The girl is carrying a baby.', 'A man is riding a horse.', 'A woman is playing violin.', 'Two men pushed carts through the woods.', 'A man is riding a white horse on an enclosed ground.', 'A monkey is playing drums.',
+        'A cheetah is running behind its prey.'
+    ]
 
-    # We use cosine-similarity and torch.topk to find the highest 5 scores
-    cos_scores = util.cos_sim(query_embedding, corpus_embeddings)[0]
-    top_results = torch.topk(cos_scores, k=top_k)
+    # Define query sentences
+    queries = ['A man is eating pasta.', 'Someone in a gorilla costume is playing a set of drums.', 'A cheetah chases prey on across a field.']
 
-    print("\n\n======================\n\n")
-    print("Query:", query)
-    print("\nTop 5 most similar sentences in corpus:")
+    # Find and print the top similar sentences for each query
+    results = find_top_similar_sentences(queries, corpus)
 
-    for score, idx in zip(top_results[0], top_results[1]):
-        print(corpus[idx], "(Score: {:.4f})".format(score))
-
-    """
-    # Alternatively, we can also use util.semantic_search to perform cosine similarty + topk
-    hits = util.semantic_search(query_embedding, corpus_embeddings, top_k=5)
-    hits = hits[0]      #Get the hits for the first query
-    for hit in hits:
-        print(corpus[hit['corpus_id']], "(Score: {:.4f})".format(hit['score']))
-    """
+    for query, query_results in results:
+        print("\n======================\n")
+        print("Query:", query)
+        print("\nTop 5 most similar sentences in the corpus:")
+        for sentence, score in query_results:
+            print(f"{sentence} (Score: {score:.4f})")
